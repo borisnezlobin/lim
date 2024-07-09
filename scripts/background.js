684e5addea78701eb4f3c2dc78dadab76c7f4d91
borisnezlobin <boris.nezlobin@gmail.com>
2024-07-09 08:51:37 -0700

    handle browser sleep and unfocus

--- scripts/background.js
+++ scripts/background.js
@@ -1,14 +1,40 @@
 let currentTab = "";
 let currentFavicon = "";
 let startTime = Date.now();
-let lastWindowId = browser.windows.WINDOW_ID_NONE;
+let currentWindowID = browser.windows.WINDOW_ID_NONE;
+let browserAwake = true;
+let lastIntervalTime = Date.now();
 
 browser.windows.onFocusChanged.addListener((abc) => {
     console.log("window focus changed", abc);
+    // iffy — browser window could be mildly sus because it could be a sidebar or something. might as well count that as "active", though
+    currentWindowID = abc;
 })
 
-// todo: every 30 seconds, update the time in the current tab
+// run this interval every 2 minutes, then if the difference is significantly greater than 2 minutes, don't count the time spent
+const time = 2 *  1000;
+setInterval(() => {
+    if(Date.now() - lastIntervalTime > time + 1 * 60 * 1000){
+        // the computer went to sleep, don't count the last one
+        browserAwake = false; // todo: mvoe this from handleNewUrl to here, maybe?
+        console.log("browser was asleep since", new Date(lastIntervalTime));
+    } else {
+        browserAwake = true;
+    }
+    lastIntervalTime = Date.now();
+}, time);
+
 setInterval(async () => {
+    // don't update current tab if the browser window is not focused
+    // todo: verify this works
+    if(!browserAwake || currentWindowID == browser.windows.WINDOW_ID_NONE){
+        console.log("not updating tiem because browserawake is", browserAwake, ", currentwindowid is", currentWindowID);
+        currentTab = "";
+        currentFavicon = "";
+        startTime = Date.now(); // or something
+        return;
+    }
+
     const tab = await browser.tabs.query({ active: true, currentWindow: true });
     console.log(tab[0]);
     handleNewUrl(getURL(tab[0].url), tab[0].favIconUrl);
@@ -16,6 +42,7 @@ setInterval(async () => {
 
 const handleNewUrl = (url, favicon) => {
     // write this time spent to storage under url's sitename
+    if(!url) return;
     let timeSpent = Date.now() - startTime;
 
     browser.storage.local.get("usage").then((result) => {
