b610c401c31cc8f6f82bce3714daf6e04f6e8f58
borisnezlobin <boris.nezlobin@gmail.com>
2024-07-08 22:11:18 -0700

    almost done with time tracking

--- scripts/background.js
+++ scripts/background.js
@@ -1,37 +1,41 @@
-let currentTab = "ur mom"
+let currentTab = "";
+let currentFavicon = "";
 let startTime = Date.now();
+let lastWindowId = browser.windows.WINDOW_ID_NONE;
+
+browser.windows.onFocusChanged.addListener((abc) => {
+    console.log("window focus changed", abc);
+})
 
 // todo: every 30 seconds, update the time in the current tab
+setInterval(async () => {
+    const tab = await browser.tabs.query({ active: true, currentWindow: true });
+    console.log(tab[0]);
+    handleNewUrl(getURL(tab[0].url), tab[0].favIconUrl);
+}, 1000);
 
 const handleNewUrl = (url, favicon) => {
-    console.log('new url:', url);
-    let urlObj = new URL(url);
-    const key = urlObj.hostname;
-    if(!key) return;
-
     // write this time spent to storage under url's sitename
     let timeSpent = Date.now() - startTime;
-    console.log('time spent:', timeSpent);
 
     browser.storage.local.get("usage").then((result) => {
-        console.log('result:', result);
-        if (!result.usage[key]) {
+        if (!result || !result.usage || !result.usage[currentTab]) {
             browser.storage.local.set({
                 usage: {
-                    ...result.usage, [key]: {
+                    ...result.usage, [currentTab]: {
                         time: timeSpent,
                         icon: favicon,
-                        url: key
+                        url: currentTab
                     }
                 }
             });
         } else {
             browser.storage.local.set({
                 usage: {
-                    ...result.usage, [key]: {
-                        time: result.usage[key].time + timeSpent,
+                    ...result.usage, [currentTab]: {
+                        time: result.usage[currentTab].time + timeSpent,
                         icon: favicon,
-                        url: key
+                        url: currentTab
                     }
                 }
             });
@@ -42,18 +46,29 @@ const handleNewUrl = (url, favicon) => {
     startTime = Date.now();
     currentTab = url;
 
-    browser.runtime.sendMessage({ newUrl: url, type: "location-change" });
+    // try {
+    //     browser.runtime.sendMessage({ newUrl: url, type: "location-change" });
+    // } catch (e) {}
 }
 
 browser.tabs.onActivated.addListener(async function (activeInfo) {
     console.log("tab activated:", activeInfo);
     let tab = await browser.tabs.get(activeInfo.tabId);
-    handleNewUrl(tab.url, tab.favIconUrl);
+    handleNewUrl(getURL(tab.url), tab.favIconUrl);
 });
 
 browser.tabs.onUpdated.addListener(async function (tabId, changeInfo, tabInfo) {
     if (changeInfo.url) {
         console.log("tab updated:", tabInfo.url);
-        handleNewUrl(tabInfo.url, tabInfo.favIconUrl);
+        handleNewUrl(getURL(tabInfo.url), tabInfo.favIconUrl);
     }
 });
+
+const getURL = (str) => {
+    try {
+        const url = (new URL(str)).hostname;
+        return url ? url : str.split("#")[0];
+    } catch (e) {
+        return str.split("#")[0];
+    }
+}
